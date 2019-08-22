88b5d39f6fba248f378ae5f563b3754bcf1a93f1
Noah Sylwester <nanoabram@comcast.net>
2019-08-21 21:51:04 -0700

    Added animations to fade main content in and out. Added functionality to display first question.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,14 +9,49 @@ var currentQuestionIndex = 0;
 var categoryId;
 var questions;
 
-$('.question-display').on("click", ".category", function() {
-  categoryId = $('this').data('number');
-})
+// function definitions
+function nextQuestion() {
+  timeRemaining = 30;
+  $('.question-display').empty().html(
+  `
+  <h2 class="time-display">
+    Time remaining: ${timeRemaining}
+  </h2>
+  <br />
+  <div>
+    ${questions.results[currentQuestionIndex].question}
+  </div>
+  <br />
+  <button class="option answer">
+    ${questions.results[currentQuestionIndex].correct_answer}
+  </button>
+  <button class="option answer">
+    ${questions.results[currentQuestionIndex].incorrect_answers[0]}
+  </button>
+  <button class="option answer">
+    ${questions.results[currentQuestionIndex].incorrect_answers[1]}
+  </button>
+  <button class="option answer">
+    ${questions.results[currentQuestionIndex].incorrect_answers[2]}
+  </button>
+  `
+  )
+}
 
-$.ajax({
-  url: `https://opentdb.com/api.php?amount=10&category=${categoryId}&difficulty=easy&type=multiple`,
-  method: "GET"
-}).then((response) => {
-  console.log(response);
-  questions = response;
-})
+
+
+$('.question-display').on("click", ".category", function() {
+  categoryId = $(this).data('number');
+  $('main').addClass('fadeout');
+  $.ajax({
+    url: `https://opentdb.com/api.php?amount=10&category=${categoryId}&difficulty=easy&type=multiple`,
+    method: "GET"
+  }).then((response) => {
+    console.log(response);
+    questions = response;
+    setTimeout(() => {
+      nextQuestion();
+      $('main').removeClass('fadeout').addClass('fadein');
+    }, 500);
+  });
+});
